e7d0893f1a3d8692a3ba9e1ccf904f37421e6f3b
hana545 <hana.radojcic545@gmail.com>
2020-03-06 00:01:25 +0100

    added occasions table

---
+++ database/migrations/2020_03_05_152016_create_occasions_table.php
@@ -0,0 +1,42 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateOccasionsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('occasions', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('name');
+            $table->string('street');
+            $table->string('city');
+            $table->string('zipcode');
+            $table->dateTime('start');
+            $table->dateTime('end');
+            $table->unsignedBigInteger('user_id')->index(); //user_id
+            $table->foreign('user_id')->references('id')->on('users');
+            $table->string('category');
+            $table->integer('max_people')->default(0);
+            $table->boolean('ended')->default(0);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('occasions');
+    }
+}
