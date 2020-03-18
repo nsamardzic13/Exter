d9c59cd4434b3d3a931bb82424758923bbc3a3a8
hana545 <hana.radojcic545@gmail.com>
2020-03-18 03:32:50 +0100

    forgot to add in previous commit

---
+++ resources/views/occasions/time.blade.php
@@ -0,0 +1,22 @@
+<div class="form-group">
+    <label for='time-start'>Start time:</label>
+    <input type="time" name="time-start"  id="start-time" value="{{ old('time-start')}}" value="08:00:AM" class="form-control">
+</div>
+<div>{{ $errors->first('time-start') }}</div>
+<div class="form-group">
+    <label for='time-end'>End time:</label>
+    <input type="time" name="time-end"  value="{{ old('time-end')}}" value="09:00:AM" class="form-control">
+</div>
+<div>{{ $errors->first('time-end') }}</div>
+<p>Repeat on these days</p>
+
+<div class="btn-group-toggle mb-2 row justify-content-around" data-toggle="buttons">
+    @foreach ($days as $day => $dayValue)
+        <label class="btn btn-outline-quest col-auto px-4">
+            <input type="checkbox" name="day"
+                   value=" {{$day}}" >
+            {{ $dayValue}}
+        </label>
+    @endforeach
+</div>
+<div>{{ $errors->first('day') }}</div>
